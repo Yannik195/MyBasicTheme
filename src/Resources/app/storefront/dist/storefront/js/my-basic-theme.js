966dbf271534af82ecb0bf6aa35c74d971cf537d
Yannik195 <yannisim@gmail.com>
2021-09-20 15:59:26 +0200

    basic theme training finished

---
+++ src/Resources/app/storefront/dist/storefront/js/my-basic-theme.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["my-basic-theme"],{
+
+/***/ "jA56":
+/*!*********************************************************************************!*\
+  !*** /app/custom/plugins/MyBasicTheme/src/Resources/app/storefront/src/main.js ***!
+  \*********************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiakE1Ni5qcyIsInNvdXJjZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///jA56\n");
+
+/***/ })
+
+},[["jA56","runtime"]]]);
